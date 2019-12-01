752d159a4dd67dc43208ae6b50aff68618ad7aa5
Dominik Vidusin <dominik.vidusin@gmail.com>
2019-12-01 23:40:36 +0100

    Use defined classes

---
+++ Flyers/src/ui/FlyerDemo.java
@@ -0,0 +1,36 @@
+package ui;
+
+import model.*;
+
+public class FlyerDemo {
+
+  // NOTE: this project goes with the Subclasses & Extends, Pt. 2 (Extending) Video
+
+  public static void main(String[] args) {
+    Bird birdie = new Bird();
+    Airplane planie = new Airplane();
+    Cafe cafePlane = new Airplane();
+    Flyer flyerPlane = new Airplane();
+
+
+    birdie.fly();
+    cafePlane.serveDrinks();
+
+    FlyerDemo fd = new FlyerDemo();
+    fd.lunchService(cafePlane);
+    fd.firstPartOfFlight(planie);
+    // fd.firstPartOfFlight(flyerPlane); - not possible, Flyer is not subclass of Airplane
+  }
+
+  public void lunchService(Cafe c){
+    c.serveDrinks();
+    c.serveSnacks();
+  }
+
+  public void firstPartOfFlight(Airplane a){
+    a.takeOff();
+    a.serveDrinks();
+  }
+
+
+}
